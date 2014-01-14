a161977419897c8cb9db42a664eaf9052593e459
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-01-14 23:26:51 +0200

    Oops, forgot to commit routes.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Ratebeer::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :breweries
+
   # Example resource route with options:
   #   resources :products do
   #     member do
