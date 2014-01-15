bce5c926b5bfa1a88401e5ee877d14964b9c4c25
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-01-15 20:51:08 +0200

    Week 1 done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ratebeer::Application.routes.draw do
+  resources :beers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114192647) do
+ActiveRecord::Schema.define(version: 20140115161524) do
+
+  create_table "beers", force: true do |t|
+    t.string   "name"
+    t.string   "style"
+    t.integer  "brewery_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "breweries", force: true do |t|
     t.string   "name"
