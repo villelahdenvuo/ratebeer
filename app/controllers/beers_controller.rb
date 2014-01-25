228fda181b36db7266c8fa10546cb1ee7196decb
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-01-25 19:27:08 +0200

    Exercise 10.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -16,6 +16,7 @@ class BeersController < ApplicationController
   def new
     @beer = Beer.new
     @breweries = Brewery.all
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
   end
 
   # GET /beers/1/edit
