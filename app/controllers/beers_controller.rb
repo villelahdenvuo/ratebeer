758f24aa5adceb987b226d84e9378f835e81cdec
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-01-25 19:24:30 +0200

    Exercise 9.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -15,6 +15,7 @@ class BeersController < ApplicationController
   # GET /beers/new
   def new
     @beer = Beer.new
+    @breweries = Brewery.all
   end
 
   # GET /beers/1/edit
@@ -28,7 +29,7 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to @beer, notice: 'Beer was successfully created.' }
+        format.html { redirect_to beers_url, notice: 'Beer was successfully created.' }
         format.json { render action: 'show', status: :created, location: @beer }
       else
         format.html { render action: 'new' }
