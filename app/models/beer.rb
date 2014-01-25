9c6f3c9f58a0fe342237a59fdf900c3e682fb898
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-01-25 19:51:43 +0200

    Exercise 14.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,6 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
-	has_many :ratings
+	has_many :ratings, dependent: :destroy
 
 	def average_rating
 		ratings.inject(0) { |sum, el| sum + el.score } / ratings.size
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ActiveRecord::Base
-	has_many :beers
+	has_many :beers, dependent: :destroy
+	has_many :ratings, through: :beers
+
+	def average_rating
+		ratings.inject(0) { |sum, el| sum + el.score } / ratings.size
+	end
 end
